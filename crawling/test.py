30339244dd89d598050b48f8369eb3e4300d40d4
Yeeun <yesung000@naver.com>
2018-12-28 20:22:24 +0900

    fix mysql

--- crawling/test.py
+++ crawling/test.py
@@ -2,16 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import random
-# list = []
-# ran_num = random.randint(0, 10)
-# for i in range(10):
-#     while ran_num in list:
-#         ran_num = random.randint(0, 10)
-#        list.append(ran_num)
-start = 1
-end = 10001
-start =1
-end = 10001
+start = 0
+end = 100
 l = [i for i in range(start, end)]
 random.shuffle(l)
 lis2=[]
@@ -26,50 +18,34 @@ for i in l:
     table = soup.find('table', {'class': 'tbl-y'})
     trs = table.find_all('tr')
     cc = soup.find('div',{'class','r5 box chart'})
+    if cc is None:
+        continue
     tt = cc.find('p')
+
     if trs[0].find('td').text == '':
          continue
     for tr in trs:
         th = tr.find('th').text
         td = tr.find('td').text.strip()
-    #    print(th)
-    #    print(td)
-        #    lis.append(th)
         lis.append(td)
 
     gram = tt.find_all('span')
     t = tt.find("strong",{'class':'blue'}).text
     tg = gram[0].text
-   # print(t)
-   # print(tg)
-    # lis.append(t)
     lis.append(tg)
     d = tt.find("strong", {'class': 'green'}).text
     dg = gram[1].text
-   # print(d)
-   # print(dg)
-    # lis.append(d)
     lis.append(dg)
     g = tt.find("strong", {'class': 'orange'}).text
     gg = gram[2].text
-   # print(g)
-   # print(gg)
-    # lis.append(g)
     lis.append(gg)
     da = tt.find("strong", {'class': 'yellow'}).text
     dag = gram[3].text
-   # print(da)
-   # print(dag)
-    # lis.append(da)
     lis.append(dag)
     n = tt.find("strong", {'class': 'purple'}).text
     ng = gram[4].text
-  #  print(n)
-   # print(ng)
-    # lis.append(n)
     lis.append(ng)
-   # print(lis)
     lis2.append(lis)
 data = pd.DataFrame(lis2)
-data.columns = ['인덱스','음식명', '단위', '칼로리', '탄수화물','단백질','지방','당류','나트륨']
-data.to_csv('음식4.csv', encoding='utf-8')
+data.columns = ['index','name', 'unit', 'cal', 'carbs','protein','fat','sugar','salt']
+data.to_csv('food6.csv', encoding='utf-8')
