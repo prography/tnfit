24c79d5931a8f1040f1b44ea8725841a0caa81d4
CHAYOUNG1017 <gjckdud1017@naver.com>
2018-12-29 14:11:46 +0900

    modify crawling

--- crawling/test.py
+++ crawling/test.py
@@ -4,19 +4,15 @@ import pandas as pd
 import random
 import pymysql
 
-db = pymysql.connect(host='ec2-13-209-17-166.ap-northeast-2.compute.amazonaws.com',user="root",passwd="gathertooweb",db='TNfit',port=3306)
-
 start =100
 end = 150
-start = 0
-end = 100
+
 l = [i for i in range(start, end)]
 random.shuffle(l)
 lis2=[]
 a=0
 for i in l:
     lis = []
-
     page = requests.get('http://www.dietshin.com/calorie/calorie-foodview.asp?cal_idx=' + str(i) + '&cal_type=F')
     pageText = page.text
     soup = BeautifulSoup(pageText, 'html.parser')
@@ -32,24 +28,8 @@ for i in l:
          continue
     lis.append(a)
     a = a + 1
-    name=""
-    gram = ""
-    cal = ""
-    cursor = db.cursor()
-    ff=0
     for tr in trs:
-        ff=ff+1
-        th = tr.find('th').text
-        td = tr.find('td').text.strip()
         lis.append(td)
-        if ff==1:
-            name = td
-        if ff==2:
-            gram = td
-        if ff==3:
-            cal=td
-
-
 
     gram = tt.find_all('span')
     t = tt.find("strong",{'class':'blue'}).text
@@ -68,13 +48,6 @@ for i in l:
     ng = gram[4].text
     lis.append(ng)
     lis2.append(lis)
-    str = 'insert into test (index,음식명,단위,칼로리,탄수화물,단백질,지방,당류,나트륨) values(%d,%s,%s,%s,%s,%s,%s,%s,%s)' % (a, "'" + dag + "'", "'" + dag + "'", "'" +dag + "'", "'" + dag + "'", "'" + dag + "'", "'" + dag + "'",
-        "'" + dag + "'", "'" + dag + "'")
-
-   # a,"'" + name + "'", "'" + gram + "'", "'" + cal + "'", "'" + tg + "'", "'" + dg + "'", "'" + gg + "'", "'" + dag + "'", "'" + ng + "'")
-
-    cursor.execute(str)
-    db.commit()
 data = pd.DataFrame(lis2)
 data.columns = ['index','name', 'unit', 'cal', 'carbs','protein','fat','sugar','salt']
 data.to_csv('food6.csv', encoding='utf-8')
