42d2a4a822e509cf06620123aadefab2f1442433
CHAYOUNG1017 <gjckdud1017@naver.com>
2019-01-05 14:34:04 +0900

    dbPpool

---
+++ tnfit/config/dbPool.js
@@ -0,0 +1,13 @@
+const mysql = require('promise-mysql');			// mysql 모듈의 promise 버전
+
+// rds 정보 입력 : hostname, username, password, default DB
+const dbConfig = {
+	host : 'ec2-13-209-17-166.ap-northeast-2.compute.amazonaws.com',
+	port : 3306,   
+	user : 'rsh',
+	password : 'gathertooweb',
+	database : 'TNfit',
+	connectionLimit : 20
+};
+
+module.exports = mysql.createPool(dbConfig);
---
+++ tnfit/routes/diet/insert.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto-promise');  
+const db = require('../../module/pool.js');
+
+
+// let requestQuery = 'INSERT INTO Interpretation (userIdx, iboardContent, iboardDate, iboardUrl, iboardTitle) VALUES (?,?,?,?,?)'
+// let requestResult = await db.queryParam_Arr(requestQuery, [userIdx, iboardContent, currentTime, iboardUrl, iboardTitle]);
