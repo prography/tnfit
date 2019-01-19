03ec3a622a89357b80d5a97a679d1c20fa16279a
Yeeun <yesung000@naver.com>
2019-01-19 13:24:35 +0900

    confilct 해결

--- tnfit/routes/diet/d_index.js
+++ tnfit/routes/diet/d_index.js
@@ -2,18 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/insert', require('./insert.js'));
-<<<<<<< HEAD
-// router.use('/showday', require('./showday.js'));
+router.use('/showday', require('./showday.js'));
 router.use('/showdiet', require('./showdiet.js'));
 router.use('/showmeal', require('./showmeal.js'));
 router.use('/searchdiet', require('./searchdiet.js'));
 router.use('/showweek', require('./showweek.js'));
-=======
-router.use('/showday', require('./showday.js'));
- router.use('/showdiet', require('./showdiet.js'));
-  router.use('/showmeal', require('./showmeal.js'));
-   router.use('/searchdiet', require('./searchdiet.js'));
-// router.use('/showweek', require('./showweek.js'));
->>>>>>> c2ef5f586ce46abdd80299c4542bd437336f95c8
 
 module.exports = router;
