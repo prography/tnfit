b48481ab74bc747a48eb04e7e3d00da702ae6934
CHAYOUNG1017 <gjckdud1017@naver.com>
2019-01-19 13:19:22 +0900

    showday

--- tnfit/routes/diet/d_index.js
+++ tnfit/routes/diet/d_index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/insert', require('./insert.js'));
-// router.use('/showday', require('./showday.js'));
+router.use('/showday', require('./showday.js'));
  router.use('/showdiet', require('./showdiet.js'));
   router.use('/showmeal', require('./showmeal.js'));
    router.use('/searchdiet', require('./searchdiet.js'));
